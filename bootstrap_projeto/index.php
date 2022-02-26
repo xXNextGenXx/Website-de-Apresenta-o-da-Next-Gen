3f3390e20bece9bb6485b7630c38e93f4758859b
Next Gen <99698542+xXNextGenXx@users.noreply.github.com>
2022-02-26 18:13:06 -0300

    Add files via upload

---
+++ bootstrap_projeto/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Next Gen</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+   
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Next Gen</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#sobre">Sobre</a></li>
+            <li><a href="#contact">Contato</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+    <div class="box">
+      <section id="home" class="banner">
+        <div class="overlay"></div>
+        <div class="container chamada-banner">
+          <div class="row">
+              <div class="col-md-12 text-center">
+                  <h2><?php echo htmlentities('<'); ?>Next Gen<?php echo htmlentities('>');  ?></h2>
+                  <p>Next Gen a Tecnologia que eleva o seu negócio a outro patamar.</p>
+                  <a href="">Saiba Mais!</a>
+              </div><!--col-md-12-->
+          </div><!--row-->
+        </div>
+      </section>
+      <section class="cadastro-lead">
+          <div class="container">
+            <div class="row text-center">
+                <div style="margin-top: -7px;" class="col-md-6">
+                  <h2><span aria-hidden="true"></span> Informe o email para entrarmos em contato:
+                   </h2>
+                </div>
+                <div style="margin-top: -3px;" class="col-md-6">
+                  <form method="post">
+                    <input type="text" name="nome" /><input type="submit" value="Enviar" />
+                  </form>
+                </div>
+            </div>
+          </div>
+      </section>
+
+      <section class="depoimento text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                  <h2>Depoimento</h2>
+                    <blockquote>
+                      <h4>"Eu amo cozinhar, desde que me entendo por gente, os bolos que eu faço despertam sentimentos e alegria naqueles que os comem, contudo desde que isso deixou de ser um hobbie e se tornou um negócio eu raramente conseguia alcançar o público que eu queria, e despertar nas novas gerações a magia de comer um bom bolo de vô, a Next Gen me ajudou a criar um Website para o meu negócio que levou minha lucratividade e alcance a patameres nunca imaginados."</h4>
+                  </blockquote>
+
+                </div>
+            </div>
+            <p style="text-align: right;">~ Boleria da Sônia</p>
+        </div>
+      </section>
+
+    <section class="diferenciais text-center">
+      <h2>Conheça nossa empresa</h2>
+        <div class="container diferenciais-container">
+            <div class="row">
+              <div class="col-md-4">
+                  <h3><span class="glyphicon glyphicon-glass"></span></h3>
+                  <h2>Diferencial #1</h2>
+                  <p>Para você que não sabe por onde começar no processo de criar um espaço digital para o seu negócio Físico e sempre sonhou em criar um grande negócio com seus talentos e se equiparar a empresas como Ifood, Uber Eats, Mercado Livre entre outras, nós da Next Gen te ensinamos a dar o primeiro passo.</p>
+              </div>
+
+              <div class="col-md-4">
+                  <h3><span class="glyphicon glyphicon-star"></span></h3>
+                  <h2>Diferencial #2</h2>
+                  <p>Se você ama a evolução tecnológica, mas não se sente confortável o suficiente para investir valores altos para ter seu próprio espaço na rede, nós da Next Gen oferecemos planos que te permitem obter Sistemas gratuitos nos 2 primeiros meses.</p>
+              </div>
+
+              <div class="col-md-4">
+                  <h3><span class="glyphicon glyphicon-heart"></span></h3>
+                  <h2>Diferencial #3</h2>
+                  <p>Você já imaginou e idealizou um serviço e pagou por ele esperando grandes resultados, mas quando o produto final foi entregue você se deparou com algo de má qualidade e muito longe do esperado, com a Next Gen você mesmo consegue acompanhar toda a linha de produção do seu WebSite e o controle sobre o estilo e o funcionamento do mesmo está nas suas mãos, tudo isso de maneira simplificada e intuitiva. </p>
+              </div>
+
+            </div>
+        </div>
+      </section>
+
+     <section class="equipe">
+        <h2>Equipe</h2>
+        <div class="container equipe-container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="equipe-single">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <div class="user-picture">
+                                </div>
+                            </div>
+                            <div class="col-md-10">
+                                <h3>Paulo Nickolas Madeiro De Sousa</h3>
+                                <p>Principal desenvolvedor da Next Gen e idealizador do serviço. Responsável por dar vida aos seus ideais no mundo virtual, o homem que não se considera apenas um programador e sim a solução para problemas que você nem sabia que tinha.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="equipe-single">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <div class="user-picture">
+                                </div>
+                            </div>
+                            <div class="col-md-10">
+                                <h3>Maria Natiele Linhares Nunes</h3>
+                                <p>Uma profissional abençoada pelas musas, seu olhar perspicaz aliado ao seu bom senso de design, tornam ela a única pessoa capaz de transformar sua ideia de negócio em uma identidade visual única de maneira intuitiva e eficiente, em poucas palavras ela faz das cores o seu Réquiem.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="equipe-single">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <div class="user-picture">
+                                </div>
+                            </div>
+                            <div class="col-md-10">
+                                <h3>Yasmim Paiva</h3>
+                                <p>O cérebro da operação e principal gerente da equipe Next Gen, leva o que conhecemos como empreendedorismo a outro nível, sempre atenta ao mercado e aos consumidores, garantindo que as soluções da Next Gen chegue a todos os usuários.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="equipe-single">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <div class="user-picture">
+                                </div>
+                            </div>
+                            <div class="col-md-10">
+                                <h3>Rafael Gomes</h3>
+                                <p>O desenvolvedor disposto a aprender e praticar tudo que for necessário em prol da produtividade e resultado, sendo franco um indíviduo singular e ousado o suficiente para se manter sempre 20passos a frente dos seus concorrentes. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>           
+            </div>
+        </div><!--equipe-container-->
+      </section>
+
+
+      <section id="contact" class="final-site">
+          <div class="container">
+              <div class="row">
+
+                  <div class="col-md-6">
+                    <h2>Fale conosco</h2>
+                    <form>
+                        <div class="form-group">
+                          <label for="email">Nome:</label>
+                          <input type="text" name="nome" class="form-control" id="nome">
+                        </div>
+
+                         <div class="form-group">
+                          <label for="email">E-mail:</label>
+                          <input type="email" name="email" class="form-control" id="email">
+                        </div>
+
+                         <div class="form-group">
+                          <label for="email">Mensagem:</label>
+                          <textarea class="form-control"></textarea>
+                        </div>
+                      
+                        <button type="submit" class="btn btn-primary">Enviar</button>
+                    </form>
+                  </div>
+
+                  <div class="col-md-6">
+                      <h2>Nossos planos</h2>
+                        <table class="table">
+                            <thead>
+                              <tr>
+                                <th>Plano Iron</th>
+                                <th>Plano Bronze</th>
+                                <th>Plano Silver</th>
+                                <th>Plano Gold</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>Duração: 2 meses sem renovação.</td>
+                                <td>Duração: 6 meses com renovação.</td>
+                                <td>Duração: 8 meses com renovação.</td>
+                                <td>Duração: 12 meses com renovação.</td>
+                              </tr>
+
+                              <tr>
+                                <td>Valor: 15%/F</td>
+                                <td>Valor: R$250,00</td>
+                                <td>Valor: R$235,00</td>
+                                <td>Valor: R$210,00</td>
+                              </tr>
+
+                               <tr>
+                                <td>Direito a Website funcionalidades Essenciais e acesso ao SAC.</td>
+                               <td>Direito a Website funcionalidades Essenciais, acesso ao SAC e app IOS.</td>
+                                <td>Direito a Website funcionalidades avançadas, acesso ao SAC e apps IOS e Android.</td>
+                                <td>Direito a Website funcionalidades avançadas, acesso ao SAC e apps IOS e Android e cursos de empreendedorismo.</td>
+                              </tr>
+                            </tbody>
+                          </table>
+                  </div>
+
+              </div>
+          </div>
+      </section>
+
+      <footer>
+        <p class="text-center">Todos os direitos reservados para Next Gen!</p>
+      </footer>
+
+    </div><!--box-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
